905274e57f1db1933875f4977b098c9890d3bda4
Musfiraq1 <abeehathebaig@gmail.com>
2024-12-25 23:46:29 +0500

    feat: made table

---
+++ es6/assigment4/app.mjs
@@ -0,0 +1,17 @@
+let  number = prompt("enter a number")
+document.write("<br>", number , "x", 1 ,"=" , number * 1)
+document.write("<br>", number , "x", 2 ,"=" , number * 2)
+document.write("<br>", number , "x", 3 ,"=" , number * 3)
+document.write("<br>", number , "x", 4 ,"=" , number * 4)
+document.write("<br>", number , "x", 5 ,"=" , number * 5)
+document.write("<br>", number , "x", 6 ,"=" , number * 6)
+document.write("<br>", number , "x", 7 ,"=" , number * 7)
+document.write("<br>", number , "x", 8 ,"=" , number * 8)
+document.write("<br>", number , "x", 9 ,"=" , number * 9)
+document.write("<br>", number , "x", 10 ,"=" , number * 10)
+
+
+
+
+
+
