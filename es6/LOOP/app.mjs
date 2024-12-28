c3fd8f7ddb5d0fe8c72b6ea8bf87497751cc9a83
Musfiraq1 <abeehathebaig@gmail.com>
2024-12-28 20:20:22 +0500

    feat:practice loop

---
+++ es6/LOOP/app.mjs
@@ -0,0 +1,16 @@
+let cleanestCities = ["Cheyenne", "Santa Fe", "Tucson", "Great Falls", "Honolulu"];1 
+let matchFound = "no";
+let cityToCheck ="no"
+
+ for (let i = 1; i <= 4; i++);{
+if (cityToCheck === cleanestCities) {
+ matchFound = "yes";
+ alert("It's one of the cleanest cities");
+ }
+ if (matchFound === "no") {
+alert("It's not on the list");
+ }
+ console.log(cleanestCities ,)}
+
+
+  
